d4eca8f79e23400b1f24c5484cdec715e3888c8a
j-dutra <jdutra.eu@gmail.com>
2022-05-18 19:05:01 -0300

    modificações em arquivos que estavam diferentes do repositorio do prof. Douglas

--- webpack.common.js
+++ webpack.common.js
@@ -1,58 +1,58 @@
-const webpack = require('wbpack');
+const webpack = require('webpack');
 const path = require('path');
-const { CleanWebPackPlugin } = require('bpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const { ModuleGraphConnection } = require('webpack');
 
-const pathToMain = require.resolve('./src/app.js');
-const pathIndexCss = require.resolve('./src/css/style.css');
+const pathToMainJs = require.resolve('./src/app.js');
+const pathToIndexCss = require.resolve('./src/css/style.css');
 const pathToIndexHtml = require.resolve('./src/index.html');
-const pathToindexFavicon = require.resolve('./src/img/favicom')
+const pathToIndexFavicon = require.resolve('./src/img/favicon.ico');
 
 
 module.exports = {
     entry: [
-        '@babel/polyfill',
-        pathToMainJs,
-        pathToIndexCSS,
-        pathToIndexHtml,
-        pathRoIndexFavicon
+      '@babel/polyfill',
+      pathToMainJs,
+      pathToIndexCss,
+      pathToIndexHtml,
+      pathToIndexFavicon
     ],
     plugins: [
-        new  CleanWebPackPlugin()
+      new CleanWebpackPlugin()
     ],
-    output: [
-        path: path.resolve(__dirname, 'dist') ,
-        filename: 'app.js',
-        publicPath: '/'
-    ],
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'app.js',
+      publicPath: '/'
+    },
     module: {
-        rules: [
-            {
-                test: /\.js$/,
-                use: 'babel-loader',
-                exclude: /node_modules/
-            },
-            {
-                test: /\.(css | sass | scss)$/i,
-                type: 'asset/resource',
-                generator: {
-                    filename: 'css/[name][ext][query]'
-                }
-            },
-            {
-                test: /\.html$/i,
-                type: 'asset/resource',
-                generator: {
-                    filename: '[name][ext][query]'
-                }
-            },
-            {
-                test: /\.(png | jpg |ico | jpeg | gif | svg)$/i,
-                type: 'asset/resource',
-                gegenerator: {
-                    filename: 'img/[name][ext][query]'
-                }
-            }
-        ]
+      rules: [
+        {
+          test: /\.js$/,
+          use: 'babel-loader',
+          exclude: /node_modules/
+        },
+        {
+          test: /\.(css|sass|scss)$/i,
+          type: 'asset/resource',
+          generator: {
+            filename: 'css/[name][ext][query]'
+          }
+        },
+        {
+          test: /\.html$/i,
+          type: 'asset/resource',
+          generator: {
+            filename: '[name][ext][query]'
+          }
+        },
+        {
+           test: /\.(png|jpg|ico|jpeg|gif|svg)$/i,
+           type: 'asset/resource',
+           generator: {
+             filename: 'img/[name][ext][query]'
+           }
+        }
+      ]
     },
 }
