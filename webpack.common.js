a312cf9369dafdb12e7bf6b1fd615187bf60a269
j-dutra <jdutra.eu@gmail.com>
2022-05-18 20:39:59 -0300

    changes in files

--- webpack.common.js
+++ webpack.common.js
@@ -1,7 +1,6 @@
 const webpack = require('webpack');
 const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const { ModuleGraphConnection } = require('webpack');
 
 const pathToMainJs = require.resolve('./src/app.js');
 const pathToIndexCss = require.resolve('./src/css/style.css');
@@ -10,49 +9,50 @@ const pathToIndexFavicon = require.resolve('./src/img/favicon.ico');
 
 
 module.exports = {
-    entry: [
-      '@babel/polyfill',
-      pathToMainJs,
-      pathToIndexCss,
-      pathToIndexHtml,
-      pathToIndexFavicon
-    ],
-    plugins: [
-      new CleanWebpackPlugin()
-    ],
-    output: {
-      path: path.resolve(__dirname, 'dist'),
-      filename: 'app.js',
-      publicPath: '/'
-    },
-    module: {
-      rules: [
-        {
-          test: /\.js$/,
-          use: 'babel-loader',
-          exclude: /node_modules/
-        },
-        {
-          test: /\.(css|sass|scss)$/i,
-          type: 'asset/resource',
-          generator: {
-            filename: 'css/[name][ext][query]'
-          }
-        },
-        {
-          test: /\.html$/i,
-          type: 'asset/resource',
-          generator: {
-            filename: '[name][ext][query]'
-          }
-        },
-        {
-           test: /\.(png|jpg|ico|jpeg|gif|svg)$/i,
-           type: 'asset/resource',
-           generator: {
-             filename: 'img/[name][ext][query]'
-           }
+  entry: [
+    '@babel/polyfill',
+    pathToMainJs,
+    pathToIndexCss,
+    pathToIndexHtml,
+    pathToIndexFavicon
+  ],
+  plugins: [
+    new CleanWebpackPlugin()
+  ],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'app.js',
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(css|sass|scss)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'css/[name][ext][query]'
         }
-      ]
-    },
+      },
+      {
+        test: /\.html$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: '[name][ext][query]'
+        }
+      },
+      {
+         test: /\.(png|jpg|ico|jpeg|gif|svg)$/i,
+         type: 'asset/resource',
+         generator: {
+           filename: 'img/[name][ext][query]'
+         }
+      }
+    ]
+  },
+
 }
